a167362d9a67f2636ba4a8eee679245c76db18bb
sandeepny441 <62777490+sandeepny441@users.noreply.github.com>
2020-06-10 22:33:20 -0500

    Update vocab_probability.py

--- vocab_probability.py
+++ vocab_probability.py
@@ -8,3 +8,25 @@ CDF
 
 Likelihood 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Probability distributions describe the dispersion of the values of a random variable. 
+Consequently, the kind of variable determines the type of probability distribution. 
