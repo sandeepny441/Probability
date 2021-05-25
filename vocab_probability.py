a79e54c95b1d1c7b08cac89f4447673c18618d6a
sandeepny441 <62777490+sandeepny441@users.noreply.github.com>
2021-05-25 00:37:39 -0500

    Update vocab_probability.py

--- vocab_probability.py
+++ vocab_probability.py
@@ -1,12 +1,40 @@
 discrete
 continuous
 
+Mean
+Median
+Mode
+Range
+Variance
+Standard Deviation
+Kurtosis 
+Moment Generating Function
+
 PMF
 PDF 
-
 CDF
 
 Likelihood 
+Bayes
+Prior
+Posterior
+Maximum Likelihood
+Negative Log-likelihood
+Sample space
+Reduced Sample space
+
+
+
+distributions:
+  binomial
+  unifrom 
+  Normal
+  geometric
+  negative_binomial
+  hyper_geometric
+  poisson
+  gamma
+  beta
 
 
 
